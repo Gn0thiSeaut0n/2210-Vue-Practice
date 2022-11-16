cacbc13ff92b631f759489f70a271ca9eb682b46
Chanbeom <kin280000@gmail.com>
2022-11-16 11:20:00 +0900

    Todo2

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -13,6 +13,8 @@
       v-for="todo in todos"
       :key="todo.id" 
       :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo"
     />
   </div>
 </template>
@@ -33,6 +35,13 @@ export default {
     }
   },
   methods: {
+    deleteTodo(id) {
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id === id;
+      // });
+      // this.todos.splice(index, 1);
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
     addTodo(e) {
       this.todos.push({
         id: Math.random(),
@@ -40,6 +49,12 @@ export default {
         checked: false
       });
       this.todoText= '';
+    },
+    toggleCheckbox({id, checked}) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
     }
   }
 }
