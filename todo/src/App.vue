4f9acaed274fd5a61015b96b319da6edb7bdfdf7
Chanbeom <kin280000@gmail.com>
2022-11-17 10:19:14 +0900

    Vuex Ready

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,33 +1,30 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <input
-      v-model="todoText"
-      type="text"
-      class="w-100 p-2"
-      placeholder="Type todo"
-      @keyup.enter="addTodo"
-    >
+    <CompletedTodo :todos="todos"/>
+    <AddTodo @add-todo="addTodo"/>
     <hr>
-    <Todo
-      v-for="todo in todos"
-      :key="todo.id" 
-      :todo="todo"
+    <TodoList
+      :todos="todos"
       @toggle-checkbox="toggleCheckbox"
       @click-delete="deleteTodo"
     />
+    {{ todos }}
   </div>
 </template>
 
 <script>
-import Todo from '@/components/Todo.vue';
+import TodoList from '@/components/TodoList.vue';
+import AddTodo from '@/components/AddTodo.vue';
+import CompletedTodo from '@/components/CompletedTodo.vue';
 export default {
   components: {
-    Todo
+    TodoList,
+    AddTodo,
+    CompletedTodo
   },
   data() {
     return {
-      todoText: '',
       todos: [
         { id:1, text: 'buy a car', checked: false },
         { id:2, text: 'play game', checked: false },
@@ -42,13 +39,12 @@ export default {
       // this.todos.splice(index, 1);
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
-    addTodo(e) {
+    addTodo(value) {
       this.todos.push({
         id: Math.random(),
-        text: e.target.value,
+        text: value,
         checked: false
       });
-      this.todoText= '';
     },
     toggleCheckbox({id, checked}) {
       const index = this.todos.findIndex(todo => {
