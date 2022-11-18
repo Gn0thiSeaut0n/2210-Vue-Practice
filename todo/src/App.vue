ecef60db5459c8fe6607237a713e4a6b43f3f343
Chanbeom <kin280000@gmail.com>
2022-11-18 14:17:48 +0900

    vuex state & mutations

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,15 +1,10 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <CompletedTodo :todos="todos"/>
-    <AddTodo @add-todo="addTodo"/>
+    <CompletedTodo />
+    <AddTodo />
     <hr>
-    <TodoList
-      :todos="todos"
-      @toggle-checkbox="toggleCheckbox"
-      @click-delete="deleteTodo"
-    />
-    {{ todos }}
+    <TodoList />
   </div>
 </template>
 
@@ -22,36 +17,6 @@ export default {
     TodoList,
     AddTodo,
     CompletedTodo
-  },
-  data() {
-    return {
-      todos: [
-        { id:1, text: 'buy a car', checked: false },
-        { id:2, text: 'play game', checked: false },
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      // const index = this.todos.findIndex(todo => {
-      //   return todo.id === id;
-      // });
-      // this.todos.splice(index, 1);
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(value) {
-      this.todos.push({
-        id: Math.random(),
-        text: value,
-        checked: false
-      });
-    },
-    toggleCheckbox({id, checked}) {
-      const index = this.todos.findIndex(todo => {
-        return todo.id === id;
-      });
-      this.todos[index].checked = checked;
-    }
   }
 }
 </script>
